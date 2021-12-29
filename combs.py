50537537a6b223b19f25c22b561d1ce3dd7b5df0
Royal4224 <roytwang1234@gmail.com>
2021-12-29 14:45:51 -0800

    Update combs.py

--- combs.py
+++ combs.py
@@ -3,9 +3,9 @@ def combs(n):
     if n == 0:
         return a
     for p in range(n + 1):
-        for nic in range(int(n / 5) + 1):
-            for d in range(int(n / 10) + 1):
-                for q in range(int(n / 25) + 1):
+        for nic in range(n // 5 + 1):
+            for d in range(n // 10 + 1):
+                for q in range(n // 25 + 1):
                     if p + nic * 5 + d * 10 + q * 25 == n:
                         a.append([p, nic, d, q])
     return a
@@ -15,21 +15,16 @@ print(combs(100))
 
 def recombs(n, a, choices):
     c = []
-    if n == 0 or len(a) == 0:
+    if n == 0 and len(a) == 0:
+        return [choices]
+    if len(a) == 0 and n != 0:
         return c
     #acopy = a.copy()
     first = a.pop(0)
-    for m in range(int(n / first) + 1):
-        if n - m * first == 0:
-            choices.append(m)
-            c.append(choices)
-            a.insert(0, first)
-            # have to insert element back to maintain coin list integrity
-            return c
-        else:
-            copy = choices.copy()
-            copy.append(m)
-            c.extend(recombs(n - m * first, a, copy))
+    for m in range(n // first + 1):
+        copy = choices.copy()
+        copy.append(m)
+        c.extend(recombs(n - m * first, a, copy))
     a.insert(0, first)
     return c
 
