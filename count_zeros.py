63561a928bd297d4d1454d22d63a8a42d3ae06f6
Royal4224 <roytwang1234@gmail.com>
2021-12-31 00:19:04 -0800

    Update count_zeros.py

--- count_zeros.py
+++ count_zeros.py
@@ -12,6 +12,7 @@ def count_zeros(list):
     for j in range(len(list) - count, len(list)):
         list[j] = 0
 
-    return list
+    return listxw
+
 
 print(count_zeros([1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0]))
