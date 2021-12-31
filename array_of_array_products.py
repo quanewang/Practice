54db9c29f77fa73e894334a12066a004554edff8
Royal4224 <roytwang1234@gmail.com>
2021-12-31 12:11:28 -0800

    Update array_of_array_products.py

--- array_of_array_products.py
+++ array_of_array_products.py
@@ -3,10 +3,18 @@ def array_of_array_products(arr):
     right_products = []
     array_products = []
     for i in range(len(arr)):
-        left_products.append(get_product(arr, 0, i))
+        if i == 0:
+            left_products.append(1)
+        else:
+            left_products.append(left_products[i - 1] * arr[i - 1])
+
+    for i in range(len(arr) - 1, -1, -1):
+        if i == len(arr) - 1:
+            right_products.append(1)
+        else:
+            right_products.insert(0, right_products[0] * arr[i + 1])
 
     for i in range(len(arr)):
-        right_products.append(get_product(arr, i + 1, len(arr)))
         array_products.append(left_products[i] * right_products[i])
 
     return array_products
