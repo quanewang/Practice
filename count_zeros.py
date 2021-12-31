224df8d5818c04e9023538fa0cd8a57034157483
Royal4224 <roytwang1234@gmail.com>
2021-12-31 00:07:44 -0800

    Update count_zeros.py

--- count_zeros.py
+++ count_zeros.py
@@ -5,19 +5,13 @@ def count_zeros(list):
     while i in range(len(list)):
         if list[i] == 0:
             count += 1
-        i = shuffle(list, i, count)
-
+        else:
+            list[i - count] = list[i]
+        i += 1
     for j in range(len(list) - count, len(list)):
         list[j] = 0
 
     return list
 
-def shuffle(list, i, count):
-    if count == 0 or list[i] == 0:
-        return i + 1
-    while list[i] != 0:
-        list[i - count] = list[i]
-        i += 1
-    return i
 
 print(count_zeros([1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0]))
