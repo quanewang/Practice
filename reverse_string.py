424c7820cb9a3a1048ca0a164738050ab194f5a3
Royal4224 <roytwang1234@gmail.com>
2021-12-30 19:59:22 -0800

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,48 +1,28 @@
 def reverse_string(str):
     n = len(str) - 1
     # reverse the entire string
-    for i in range(0, len(str) // 2):
-        temp = str[i]
-        str[i] = str[n - i]
-        str[n - i] = temp
-
-    print(str)
-    #end = len(str) - 1
-
-    #for i in range(len(str)).__reversed__():
-        #if str[i] == ' ':
-            #beg = i + 1
-            #for j in range(beg, (end + beg) // 2 + 1):
-               # opposite = (end - j) + beg
-              #  temp = str[j]
-              #  str[j] = str[opposite]
-              #  str[opposite] = temp
-           # end = i - 1
+    reverse_helper(str, 0, n)
 
     beg = 0
     for i in range(len(str)):
         if str[i] == ' ':
             end = i - 1
-            for j in range(beg, (end + beg) // 2 + 1):
-                opposite = (end - j) + beg
-                temp = str[j]
-                str[j] = str[opposite]
-                str[opposite] = temp
+            reverse_helper(str, beg, end)
             beg = i + 1
 
         elif i == len(str) - 1:
             end = i
-            for j in range(beg, (end + beg) // 2 + 1):
-                opposite = (end - j) + beg
-                temp = str[j]
-                str[j] = str[opposite]
-                str[opposite] = temp
+            reverse_helper(str, beg, end)
 
     return str
 
 
-def reverse_entire_string(str, beg, end):
-    pass
+def reverse_helper(str, beg, end):
+    for j in range(beg, (end + beg) // 2 + 1):
+        opposite = (end - j) + beg
+        temp = str[j]
+        str[j] = str[opposite]
+        str[opposite] = temp
 
 
 print(reverse_string(['g', 'o', ' ', 'w', 'i', 't', 'h', 'o', 'u', 't', ' ', 'w', 'i', 'n', 'd']))
