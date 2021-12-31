73d328a829fcc534ea146cda9486f3b182022d1c
Royal4224 <roytwang1234@gmail.com>
2021-12-31 13:06:49 -0800

    Update shortest_word_edit.py

--- shortest_word_edit.py
+++ shortest_word_edit.py
@@ -3,23 +3,28 @@ def shortest_word_edit(source, target, words):
         return -1
 
     diff = [(source, 0)]
+    used = []
     while diff:
         (source, count) = diff.pop(0)
         if source == target:
             return count
 
-        diff.extend(compute_diff(source, count, words))
+        diff.extend(compute_diff(source, count, words, used))
+
+    return -1
 
 
-def compute_diff(source, count, words):
+def compute_diff(source, count, words, used):
     diff = []
     for i in range(len(words)):
         one = 0
-        for j in range(len(source)):
-            if source[j] != words[i][j]:
-                one += 1
-        if one == 1:
-            diff.append((words[i], count + 1))
+        if words[i] not in used:
+            for j in range(len(source)):
+                if source[j] != words[i][j]:
+                    one += 1
+            if one == 1:
+                diff.append((words[i], count + 1))
+                used.append(words[i])
     return diff
 
 
