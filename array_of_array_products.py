558d3a1336ca5905fe1405ebd3e1d81cf600beab
Royal4224 <roytwang1234@gmail.com>
2021-12-31 12:17:33 -0800

    Update array_of_array_products.py

--- array_of_array_products.py
+++ array_of_array_products.py
@@ -20,12 +20,5 @@ def array_of_array_products(arr):
     return array_products
 
 
-def get_product(arr, beg, end):
-    product = 1
-    for i in range(beg, end):
-        product *= arr[i]
-    return product
-
-
 print(array_of_array_products([2, 7, 3, 4]))
 print(array_of_array_products([8, 10, 2]))
