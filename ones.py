0ab603aea7d69fad01907d38ee05b6b642123216
Royal4224 <roytwang1234@gmail.com>
2021-12-30 16:56:56 -0800

    Update ones.py

--- ones.py
+++ ones.py
@@ -1,4 +1,4 @@
-def count_ones(n, unit_place=0, digits=[]):
+def count_ones(n, unit_place=0, running_sum=0):
     if n == 0:
         return 0
     digit = n % 10
@@ -10,16 +10,12 @@ def count_ones(n, unit_place=0, digits=[]):
 
     if digit == 1:
         places = unit_place - 1
-        dig = 0
-        for i in range(unit_place):
-            dig += digits[i] * pow(10, places)
-            places -= 1
-        count += dig + 1
 
+        count += running_sum + 1
 
-    digits.insert(0, digit)
+    running_sum += digit * pow(10, unit_place)
     unit_place += 1
-    return count + count_ones(n // 10, unit_place, digits)
+    return count + count_ones(n // 10, unit_place, running_sum)
 
 
 print(count_ones(6189))
