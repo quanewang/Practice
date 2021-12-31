6a9284c89d093164c436c21e69996ab59ccc0393
Royal4224 <roytwang1234@gmail.com>
2021-12-31 15:32:46 -0800

    Update find_pairs_with_given_difference.py

--- find_pairs_with_given_difference.py
+++ find_pairs_with_given_difference.py
@@ -1,2 +1,11 @@
 def find_pairs_with_given_difference(arr, k):
-    pass
+    pairs = []
+    for x in range(len(arr)):
+        for y in range(len(arr)):
+            if arr[x] - arr[y] == k:
+                pairs.append([arr[x], arr[y]])
+    return pairs
+
+
+print(find_pairs_with_given_difference([0, -1, -2, 2, 1], 1))
+print(find_pairs_with_given_difference([1, 7, 5, 3, 32, 17, 12], 17))
