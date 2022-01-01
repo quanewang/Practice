82a1d374238c33da381acc9956f9b2e4f40699b1
Royal4224 <roytwang1234@gmail.com>
2022-01-01 13:52:26 -0800

    Update shifted_arr_search.py

--- shifted_arr_search.py
+++ shifted_arr_search.py
@@ -1,5 +1,12 @@
 def shifted_arr_search(arr, num):
     shift_index = find_shift(arr, 0, len(arr) - 1)
+    if not shift_index == len(arr) - 1:
+        if arr[shift_index] > arr[shift_index + 1]:
+            shift_index += 1
+    elif not shift_index == 0:
+        if not arr[shift_index] < arr[shift_index - 1]:
+            shift_index -= 1
+
     print("shift: " + str(shift_index))
     num_index = search(arr, 0, shift_index - 1, num)
 
@@ -25,13 +32,13 @@ def find_shift(arr, beg, end):
     mid = (beg + end) // 2
     if beg >= end:
         return mid
-    if arr[mid] > arr[end]:
+    if arr[mid] > arr[len(arr) - 1]:
         return find_shift(arr, mid + 1, end)
     else:
-        return find_shift(arr, beg, mid)
+        return find_shift(arr, beg, mid - 1)
 
 
-print(shifted_arr_search([9, 12, 17, 2, 4, 5], 5))
+print(shifted_arr_search([9, 12, 17, 2, 4, 5, 8], 5))
 print(shifted_arr_search([2, 1], 1))
 print(shifted_arr_search([5, 2, 3], 5))
-print(shifted_arr_search([5, 2, 3, 4], 5))
+print(shifted_arr_search([5, 6, 1, 2, 3], 5))
