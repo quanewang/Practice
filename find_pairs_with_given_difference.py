7e6ce5d8bfb27a0c549f35fcdaed767b3552d235
Royal4224 <roytwang1234@gmail.com>
2021-12-31 16:42:57 -0800

    Update find_pairs_with_given_difference.py

--- find_pairs_with_given_difference.py
+++ find_pairs_with_given_difference.py
@@ -17,11 +17,6 @@ def find_pairs_with_given_difference(arr, k):
 
     return pairs
 
-def get(curr, complement, elements, pairs):
-    val = elements.get(complement)
-    if val is not None:
-        pairs.insert(val, [curr, complement])
-    return
 
 print(find_pairs_with_given_difference([0, -1, -2, 2, 1], 1))
 print(find_pairs_with_given_difference([1, 7, 5, 3, 32, 17, 12], 17))
