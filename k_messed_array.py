676a04ac3db54c856e9f6b517aa94482b5274c4c
Royal4224 <roytwang1234@gmail.com>
2022-01-01 00:56:17 -0800

    Update k_messed_array.py

--- k_messed_array.py
+++ k_messed_array.py
@@ -2,16 +2,19 @@ import heapq
 
 def k_messed_array(arr, k):
     q = []
-    for i in range(k + 1):
-        heapq.heappush(q, arr[i])
+    result = []
 
     for i in range(len(arr)):
-        arr[i] = heapq.heappop(q)
-        next_index = i + k + 1
-        if next_index < len(arr):
-            heapq.heappush(q, arr[next_index])
+        if len(q) < k + 1:
+            heapq.heappush(q, arr[i])
 
-    return arr
+        elif len(q) == k + 1:
+            result.append(heapq.heappop(q))
+            heapq.heappush(q, arr[i])
+
+    while q:
+        result.append(heapq.heappop(q))
+    return result
 
 
 print(k_messed_array([1, 4, 5, 2, 3, 7, 8, 6, 10, 9], 2))
