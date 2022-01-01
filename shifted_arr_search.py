2b41c26b2a7c9c5c0218c99e96cb5821d7e46886
Royal4224 <roytwang1234@gmail.com>
2022-01-01 13:16:29 -0800

    Update shifted_arr_search.py

--- shifted_arr_search.py
+++ shifted_arr_search.py
@@ -1,38 +1,34 @@
 def shifted_arr_search(arr, num):
-    mid_index = (len(arr) - 1) // 2
-    shift_index = find_shift(arr, 0, len(arr) - 1, mid_index)
+    shift_index = find_shift(arr, 0, len(arr) - 1)
 
-    mid_index = (shift_index - 1) // 2
-    num_index = search(arr, 0, shift_index - 1, mid_index, num)
+    num_index = search(arr, 0, shift_index - 1, num)
+    
     if num_index == -1:
-        mid_index = (shift_index + len(arr) - 1) // 2
-        num_index = search(arr, shift_index, len(arr) - 1, mid_index, num)
+        num_index = search(arr, shift_index, len(arr) - 1, num)
 
     return num_index
 
 
-def search(arr, beg, end, mid, num):
+def search(arr, beg, end, num):
+    mid = (beg + end) // 2
     if arr[mid] == num:
         return mid
     elif beg >= end:
         return -1
     if arr[mid] < num:
-        new_mid = (mid + 1 + end) // 2
-        return search(arr, mid + 1, end, new_mid, num)
+        return search(arr, mid + 1, end, num)
     else:
-        new_mid = (beg + mid - 1) // 2
-        return search(arr, beg, mid - 1, new_mid, num)
+        return search(arr, beg, mid - 1, num)
 
 
-def find_shift(arr, beg, end, mid):
+def find_shift(arr, beg, end):
+    mid = (beg + end) // 2
     if beg >= end:
         return mid
     if arr[mid] > arr[end]:
-        new_mid = (mid + 1 + end) // 2
-        return find_shift(arr, mid + 1, end, new_mid)
+        return find_shift(arr, mid + 1, end)
     else:
-        new_mid = (beg + mid - 1) // 2
-        return find_shift(arr, beg, mid - 1, new_mid)
+        return find_shift(arr, beg, mid - 1)
 
 
 print(shifted_arr_search([9, 12, 17, 2, 4, 5], 2))
