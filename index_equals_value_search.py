ac801dab3bfd7f325b2424d96bdeb0428efb66a1
Royal4224 <roytwang1234@gmail.com>
2022-01-01 14:22:04 -0800

    Update index_equals_value_search.py

--- index_equals_value_search.py
+++ index_equals_value_search.py
@@ -4,6 +4,11 @@ def index_equals_value_search(arr):
 
 def search(arr, beg, end, lowest_index):
     mid = (beg + end) // 2
+    if mid == arr[mid]:
+        if lowest_index == -1:
+            lowest_index = mid
+        elif lowest_index > mid:
+            lowest_index = mid
     if beg >= end:
         return lowest_index
     if mid > arr[mid]:
@@ -11,12 +16,9 @@ def search(arr, beg, end, lowest_index):
     if mid < arr[mid]:
         return search(arr, beg, mid - 1, lowest_index)
     else:
-        if lowest_index == -1:
-            lowest_index = mid
-        elif lowest_index > mid:
-            lowest_index = mid
         return search(arr, beg, mid - 1, lowest_index)
 
 
 print(index_equals_value_search([-8, 0, 2, 5, 4, 6]))
 print(index_equals_value_search([-1, 0, 3, 6, 7]))
+print(index_equals_value_search([0, 1, 2, 3, 4, 5, 6, 7]))
