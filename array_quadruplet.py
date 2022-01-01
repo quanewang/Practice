ccaebe454dc5339d3a5a26942f16ddb816683f60
Royal4224 <roytwang1234@gmail.com>
2021-12-31 16:40:47 -0800

    practice

---
+++ array_quadruplet.py
@@ -0,0 +1,2 @@
+def array_quadruplet(arr, s):
+    pass
--- find_pairs_with_given_difference.py
+++ find_pairs_with_given_difference.py
@@ -1,11 +1,27 @@
 def find_pairs_with_given_difference(arr, k):
     pairs = []
+    elements = {}
     for x in range(len(arr)):
-        for y in range(len(arr)):
-            if arr[x] - arr[y] == k:
-                pairs.insert(y, [arr[x], arr[y]])
+        complement = arr[x] - k
+        other_complement = arr[x] + k
+
+        val = elements.get(complement)
+        if val is not None:
+            pairs.insert(val, [arr[x], complement])
+
+        val = elements.get(other_complement)
+        if val is not None:
+            pairs.insert(val, [other_complement, arr[x]])
+
+        elements.update({arr[x]: x})
+
     return pairs
 
+def get(curr, complement, elements, pairs):
+    val = elements.get(complement)
+    if val is not None:
+        pairs.insert(val, [curr, complement])
+    return
 
 print(find_pairs_with_given_difference([0, -1, -2, 2, 1], 1))
 print(find_pairs_with_given_difference([1, 7, 5, 3, 32, 17, 12], 17))
