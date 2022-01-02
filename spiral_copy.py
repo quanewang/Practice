98fa5f0e7657271502eb250f7ed7cc85f7c61c0b
Royal4224 <roytwang1234@gmail.com>
2022-01-01 21:30:42 -0800

    Update spiral_copy.py

--- spiral_copy.py
+++ spiral_copy.py
@@ -4,9 +4,13 @@ def spiral_copy(arr):
 
 def get_spiral(arr, start_row, end_row, start_col, end_col):
     spiral = []
-    if start_row >= end_row and start_col >= end_col:
+    if start_row == end_row and start_col == end_col:
         spiral.append(arr[start_row][start_col])
         return spiral
+    
+    elif start_row >= end_row or start_col >= end_col:
+        return spiral
+
 
     for i in range(start_col, end_col):
         spiral.append(arr[start_row][i])
@@ -14,14 +18,13 @@ def get_spiral(arr, start_row, end_row, start_col, end_col):
     for i in range(start_row, end_row):
         spiral.append(arr[i][end_col])
 
-    for i in range(end_col, start_col, -1):
-        spiral.append(arr[end_row][i])
+    if end_col - start_col != 1:
+        for i in range(end_col, start_col, -1):
+            spiral.append(arr[end_row][i])
 
     for i in range(end_row, start_row, -1):
         spiral.append(arr[i][start_col])
 
-    print(spiral)
-    print((start_row, end_row, start_col, end_col))
     spiral.extend(get_spiral(arr, start_row + 1, end_row - 1, start_col + 1, end_col - 1))
     return spiral
 
@@ -32,3 +35,6 @@ print(spiral_copy([[1, 2, 3, 4, 5],
                    [6, 7, 8, 9, 10],
                    [11, 12, 13, 14, 15],
                    [16, 17, 18, 19, 20]]))
+print(spiral_copy([[1, 2, 3],
+                   [6, 7, 8],
+                   [11, 12, 13]]))
