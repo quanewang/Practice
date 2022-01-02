ade83d916763ad3b568697f78712182cc4541836
Royal4224 <roytwang1234@gmail.com>
2022-01-01 23:34:24 -0800

    Update rearrange_array.py

--- rearrange_array.py
+++ rearrange_array.py
@@ -12,4 +12,37 @@ def rearrange_array(arr):
     return arr
 
 
+def extra_space_rearrange(arr):
+    available = [0]
+    for i in range(1, len(arr)):
+        available.append(1)
+
+    middle = len(arr) // 2
+    j = 1
+
+    for i in range(1, len(arr)):
+        j = i
+        pointed_to = arr[j]
+        while available[i]:
+            if j < middle:
+                if 2 * j < len(arr):
+                    available[2 * j] = 0
+                    temp = arr[2 * j]
+                    arr[2 * j] = pointed_to
+                    j *= 2
+                    pointed_to = temp
+            else:
+                b_number = j - middle
+                index = b_number * 2 + 1
+                    
+                available[index] = 0
+                temp = arr[index]
+                arr[index] = pointed_to
+                j = index
+                pointed_to = temp
+    return arr
+
+
 print(rearrange_array(["a0", "a1", "a2", "a3", "b0", "b1", "b2", "b3"]))
+
+print(extra_space_rearrange(["a0", "a1", "a2", "a3", "b0", "b1", "b2", "b3"]))
