564fa4e17162763da52dc7edab0e115fa966aa3d
Royal4224 <roytwang1234@gmail.com>
2022-01-03 13:16:50 -0800

    Update bracket_match.py

--- bracket_match.py
+++ bracket_match.py
@@ -1,8 +1,10 @@
 def bracket_match(text):
-    stack = [text[0]]
-    for i in range(1, len(text)):
+    stack = []
+    for i in range(len(text)):
         char = text[i]
-        if char == ')' and stack[len(stack) - 1] == '(':
+        if len(stack) == 0:
+            stack.append(char)
+        elif char == ')' and stack[len(stack) - 1] == '(':
             stack.pop(len(stack) - 1)
         else:
             stack.append(char)
