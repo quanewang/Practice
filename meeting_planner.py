0381858b6b46b988903f0b80c6273d032c557071
Royal4224 <roytwang1234@gmail.com>
2022-01-03 14:56:13 -0800

    Update meeting_planner.py

--- meeting_planner.py
+++ meeting_planner.py
@@ -2,20 +2,16 @@ def meeting_planner(slotsA, slotsB, dur):
     i, j = 0, 0
     while i < len(slotsA) and j < len(slotsB):
         a_beg, a_end, b_beg, b_end = slotsA[i][0], slotsA[i][1], slotsB[j][0], slotsB[j][1]
-        smallest = min(a_end, b_end)
+        smallest_end = min(a_end, b_end)
+        biggest_beg = max(a_beg, b_beg)
         if a_beg == b_beg:
-            if verify_difference(a_beg, smallest, dur):
+            if verify_difference(a_beg, smallest_end, dur):
                 return [a_beg, a_beg + dur]
+        if biggest_beg < smallest_end:
+            if verify_difference(biggest_beg, smallest_end, dur):
+                return [biggest_beg, biggest_beg + dur]
 
-        elif b_beg < a_beg < b_end:
-            if verify_difference(a_beg, smallest, dur):
-                return [a_beg, a_beg + dur]
-
-        elif a_beg < b_beg < a_end:
-            if verify_difference(a_beg, smallest, dur):
-                return [b_beg, b_beg + dur]
-
-        if smallest == a_end:
+        if smallest_end == a_end:
             i += 1
         else:
             j += 1
@@ -33,10 +29,8 @@ slotsB = [[0, 15], [60, 70]]
 dur = 8
 print(meeting_planner(slotsA, slotsB, dur))
 
-
-slotsA = [[10, 50], [60, 120], [140, 210]]
-slotsB = [[0, 15], [60, 70]]
 dur = 12
 print(meeting_planner(slotsA, slotsB, dur))
 
-
+dur = 5
+print(meeting_planner(slotsA, slotsB, dur))
