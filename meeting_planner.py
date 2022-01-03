d698e122caeb2ea55f0db83a3643792e4c500b77
Royal4224 <roytwang1234@gmail.com>
2022-01-03 14:57:21 -0800

    Update meeting_planner.py

--- meeting_planner.py
+++ meeting_planner.py
@@ -2,21 +2,21 @@ def meeting_planner(slotsA, slotsB, dur):
     i, j = 0, 0
     while i < len(slotsA) and j < len(slotsB):
         a_beg, a_end, b_beg, b_end = slotsA[i][0], slotsA[i][1], slotsB[j][0], slotsB[j][1]
-        smallest_end = min(a_end, b_end)
-        biggest_beg = max(a_beg, b_beg)
+        max_beg = max(a_beg, b_beg)
+        min_end = min(a_end, b_end)
 
         if a_beg == b_beg:
-            if verify_difference(a_beg, smallest_end, dur):
+            if verify_difference(a_beg, min_end, dur):
                 return [a_beg, a_beg + dur]
-        if biggest_beg < smallest_end:
-            if verify_difference(biggest_beg, smallest_end, dur):
-                return [biggest_beg, biggest_beg + dur]
+        if max_beg < min_end:
+            if verify_difference(max_beg, min_end, dur):
+                return [max_beg, max_beg + dur]
 
-        if smallest_end == a_end:
+        if min_end == a_end:
             i += 1
         else:
             j += 1
-            
+
     return []
 
 
