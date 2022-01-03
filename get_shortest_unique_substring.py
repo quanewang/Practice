114bb8e64cc97a8ae042795ee486aea2cfae7c22
Royal4224 <roytwang1234@gmail.com>
2022-01-02 23:01:44 -0800

    Update get_shortest_unique_substring.py

--- get_shortest_unique_substring.py
+++ get_shortest_unique_substring.py
@@ -23,22 +23,6 @@ def get_shortest_unique_substring(arr, s):
     return substring
 
 
-def helper_parse(arr, s, occurrence_dict, i, j, substring):
-    update(occurrence_dict, s[i], 1)
-    substring, j = check(arr, occurrence_dict, i, j, substring, s)
-    return substring, j
-
-
-def check(arr, occurrence_dict, i, j, substring, s, distincts):
-    while check_helper(occurrence_dict, arr, len(arr)):
-        if len(substring) == 0 or len(substring) > (i - j):
-            substring = s[j:i + 1]
-        removed = s[j]
-        j += 1
-        update(occurrence_dict, removed, -1, distincts)
-    return substring, j
-
-
 def update(occurrence_dict, element, increment, distinct):
     occurs = occurrence_dict.get(element)
     incremented = occurs + increment
@@ -50,12 +34,6 @@ def update(occurrence_dict, element, increment, distinct):
 
     return distinct
 
-
-def check_helper(occurrence_dict, arr, length):
-    for i in range(length):
-        if occurrence_dict.get(arr[i]) < 1:
-            return False
-    return True
 
 arr = ['x', 'y', 'z']
 str = "xyyzyzyx"
