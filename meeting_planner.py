3c9ec37585ba36a510dbe8b3bb8e8df4871a6a77
Royal4224 <roytwang1234@gmail.com>
2022-01-03 13:57:20 -0800

    Create meeting_planner.py

---
+++ meeting_planner.py
@@ -0,0 +1,50 @@
+def meeting_planner(slotsA, slotsB, dur):
+    i, j = 0, 0
+    while i < len(slotsA) and j < len(slotsB):
+        a_beg, a_end, b_beg, b_end = slotsA[i][0], slotsA[i][1], slotsB[j][0], slotsB[j][1]
+        smallest = get_smallest(a_end, b_end)
+        if a_beg == b_beg:
+            if verify_difference(a_beg, smallest, dur):
+                return [a_beg, a_beg + dur]
+
+        elif b_beg < a_beg < b_end:
+            smallest = get_smallest(a_end, b_end)
+            if verify_difference(a_beg, smallest, dur):
+                return [a_beg, a_beg + dur]
+
+        elif a_beg < b_beg < a_end:
+            smallest = get_smallest(a_end, b_end)
+            if verify_difference(a_beg, smallest, dur):
+                return [b_beg, b_beg + dur]
+
+        if smallest == a_end:
+            i += 1
+        else:
+            j += 1
+    return []
+
+
+def verify_difference(beg, end, dur):
+    if end - beg >= dur:
+        return True
+    return False
+
+
+def get_smallest(i, j):
+    if j < i:
+        return j
+    return i
+
+
+slotsA = [[10, 50], [60, 120], [140, 210]]
+slotsB = [[0, 15], [60, 70]]
+dur = 8
+print(meeting_planner(slotsA, slotsB, dur))
+
+
+slotsA = [[10, 50], [60, 120], [140, 210]]
+slotsB = [[0, 15], [60, 70]]
+dur = 12
+print(meeting_planner(slotsA, slotsB, dur))
+
+
