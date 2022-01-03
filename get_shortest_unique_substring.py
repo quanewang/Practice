8d95ce10a03ffd4856d4dc8ee5d87b5560bc926c
Royal4224 <roytwang1234@gmail.com>
2022-01-02 22:17:57 -0800

    Update get_shortest_unique_substring.py

--- get_shortest_unique_substring.py
+++ get_shortest_unique_substring.py
@@ -10,24 +10,26 @@ def get_shortest_unique_substring(arr, s):
     queue = []
     temp_string = ""
     for i in range(len(s)):
-        (temp_string, substring) = helper_parse(arr, s, occurrence_dict, queue, temp_string, i, substring)
-        for j in range(i + 1, len(s)):
-            (temp_string, substring) = helper_parse(arr, s, occurrence_dict, queue, temp_string, j, substring)
+        temp_string, substring = helper_parse(arr, s, occurrence_dict, queue, temp_string, i, substring)
 
     return substring
 
 
 def helper_parse(arr, s, occurrence_dict, queue, temp_string, i, substring):
-    if check(occurrence_dict, arr, len(arr)):
+    queue.append(s[i])
+    temp_string = temp_string + s[i]
+    update(occurrence_dict, s[i], 1)
+    temp_string, substring = check(arr, occurrence_dict, substring, temp_string, queue)
+    return temp_string, substring
+
+
+def check(arr, occurrence_dict, substring, temp_string, queue):
+    while check_helper(occurrence_dict, arr, len(arr)):
         if len(substring) == 0 or len(substring) > len(temp_string):
             substring = temp_string
         removed = queue.pop(0)
         update(occurrence_dict, removed, -1)
         temp_string = temp_string.replace(temp_string[0], "", 1)
-    else:
-        queue.append(s[i])
-        temp_string = temp_string + s[i]
-        update(occurrence_dict, s[i], 1)
     return temp_string, substring
 
 
@@ -36,7 +38,7 @@ def update(occurrence_dict, element, increment):
     occurrence_dict.update({element: occurs + increment})
 
 
-def check(occurrence_dict, arr, length):
+def check_helper(occurrence_dict, arr, length):
     for i in range(length):
         if occurrence_dict.get(arr[i]) < 1:
             return False
@@ -44,5 +46,8 @@ def check(occurrence_dict, arr, length):
 
 arr = ['x', 'y', 'z']
 str = "xyyzyzyx"
+
+print(get_shortest_unique_substring(arr, str))
+str = "xyyzxzxy"
 
 print(get_shortest_unique_substring(arr, str))
