80a30d9d14907bfdb73d3a7ceace1f1548f22e86
Royal4224 <roytwang1234@gmail.com>
2022-01-03 23:48:49 -0800

    Update deletion_distance.py

--- deletion_distance.py
+++ deletion_distance.py
@@ -5,19 +5,17 @@ def deletion_distance(str1, str2):
 
     while len(queue):
         copy_1, copy_2, deletion_count = queue.pop(0)
-        while len(copy_1) and len(copy_2) and copy_1[0] == copy_2[0]:
+        while copy_1 and copy_2 and copy_1[0] == copy_2[0]:
             copy_1 = copy_1.replace(copy_1[0], "", 1)
             copy_2 = copy_2.replace(copy_2[0], "", 1)
 
-        if (copy_1 == copy_2) or (not len(copy_1) and not len(copy_2)):
+        if copy_1 == copy_2:
             return deletion_count
 
-        if len(copy_1):
-            new_copy_1 = copy_1.replace(copy_1[0], "", 1)
-            queue.append((new_copy_1, copy_2, deletion_count + 1))
-        if len(copy_2):
-            new_copy_2 = copy_2.replace(copy_2[0], "", 1)
-            queue.append((copy_1, new_copy_2, deletion_count + 1))
+        if copy_1:
+            queue.append((copy_1[1:len(copy_1)], copy_2, deletion_count + 1))
+        if copy_2:
+            queue.append((copy_1, copy_2[1:len(copy_2)], deletion_count + 1))
     return 0
 
 
