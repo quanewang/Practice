869db5c68f7f05d1cc0cd38d2d5a0411f5f57e79
Royal4224 <roytwang1234@gmail.com>
2022-01-04 15:14:54 -0800

    Update root.py

--- root.py
+++ root.py
@@ -2,18 +2,19 @@ import math
 
 
 def root(x, n):
-    return root_helper(x, n, x / 2, x / 2, x)
+    return root_helper(x, n, 0, x)
 
 
-def root_helper(x, n, guess, start, end):
+def root_helper(x, n, start, end):
+    guess = (start + end) / 2
     guess_pow = math.pow(guess, n)
     if abs(x - guess_pow) <= 0.001:
         return guess
-    increment = (end - guess) / 2
     if guess_pow < x - 0.001:
-        return root_helper(x, n, guess + increment, guess, end)
+        return root_helper(x, n, guess, end)
     else:
-        return root_helper(x, n, guess - increment, start, guess)
+        return root_helper(x, n, start, guess)
 
 
 print(root(2, 2))
+print(root(8, 4))
