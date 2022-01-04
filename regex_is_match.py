c58db2028afae891660005a26517671019fb4e95
Royal4224 <roytwang1234@gmail.com>
2022-01-03 17:42:57 -0800

    Update regex_is_match.py

--- regex_is_match.py
+++ regex_is_match.py
@@ -12,23 +12,27 @@ def match_helper(text, pattern, i=0, j=0):
         return match
     char = pattern[j]
     text_char = text[i]
-
     if j + 1 < len(pattern) and pattern[j + 1] == '*':
-        if char != text_char:
-            j += 2
-        else:
-            match = match_helper(text[i + 1:len(text)], pattern[j:len(pattern)])
-            if match:
-                return match
-            return match_helper(text[i:len(text)], pattern[j + 2:len(pattern)])
-    elif char == "." or char == text_char:
+        match = match_helper(text[i:len(text)], pattern[j + 2:len(pattern)])
+        if match:
+            return match
+
+        return match_helper(text[i + 1:len(text)], pattern[j:len(pattern)])
+    elif compare(char, text_char):
         i += 1
         j += 1
-    elif char != text_char:
+    else:
         return False
 
     match = match_helper(text, pattern, i, j)
     return match
+
+
+def compare(char, other):
+    if char == '.' or other == '.':
+        return True
+    return char == other
+
 
 text = "acd"
 pattern = "ab*cd"
@@ -52,3 +56,6 @@ text = "abcd"
 pattern = "ab.d"
 print(is_match(text, pattern))
 
+text = "acxbc"
+pattern = "a.*bc"
+print(is_match(text, pattern))
