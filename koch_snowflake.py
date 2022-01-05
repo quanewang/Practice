f11d73368eeac2b2d4cf6597c10a1b0c39032bee
Royal4224 <roytwang1234@gmail.com>
2022-01-04 18:12:35 -0800

    Update koch_snowflake.py

--- koch_snowflake.py
+++ koch_snowflake.py
@@ -3,9 +3,10 @@ def koch_snowflake(length=1, depth=0):
     count = 0
     while i < 3:
         count += draw_helper(length, depth)
-        print("turn 120 degrees right")
+        turn(120, "right")
         i += 1
     return count // 4
+
 
 def draw_helper(length, depth):
     count = 0
@@ -14,15 +15,17 @@ def draw_helper(length, depth):
         return count + 1
     print()
     count += draw_helper(length, depth - 1)
-    print("turn 60 degrees left")
+    turn(60, "left")
     count += draw_helper(length, depth - 1)
-    print("turn 120 degrees right")
+    turn(120, "right")
     count += draw_helper(length, depth - 1)
-    print("turn 60 degrees left")
+    turn(60, "left")
     count += draw_helper(length, depth - 1)
     print()
     return count
 
+def turn(degrees, direction):
+    print("turn " + str(degrees) + " degrees " + direction)
 
 
 print(koch_snowflake(9, 3))
