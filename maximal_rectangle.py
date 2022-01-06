48616c6b7cfc9139a8b40fa4bcf5dae8155a63df
Royal4224 <roytwang1234@gmail.com>
2022-01-05 16:36:44 -0800

    Update maximal_rectangle.py

--- maximal_rectangle.py
+++ maximal_rectangle.py
@@ -1,36 +1,40 @@
 def maximal_rectangle(matrix):
     max_sum = 0
-    sums_dict = {}
+    sum_arr = []
+    for row in range(len(matrix)):
+        sum_arr.append([0] * len(matrix[0]))
     for start_row in range(len(matrix)):
         for start_col in range(len(matrix[0])):
             for end_row in range(start_row, len(matrix)):
                 for end_col in range(start_col, len(matrix[0])):
-                    rectangle_sum = get_sum(matrix, start_row, start_col, end_row, end_col, sums_dict)
+                    rectangle_sum = get_sum(matrix, start_row, start_col, end_row, end_col, sum_arr)
                     if rectangle_sum == (end_row + 1 - start_row) * (end_col + 1 - start_col):
                         if rectangle_sum > max_sum:
                             max_sum = rectangle_sum
+    for row in sum_arr:
+        print(row)
     return max_sum
 
 
-def get_sum(matrix, start_row, start_col, end_row, end_col, sums_dict):
-    if (end_row, end_col) in sums_dict:
-        rectangle_sum = sums_dict.get((end_row, end_col))
+def get_sum(matrix, start_row, start_col, end_row, end_col, sum_arr):
+    if start_row == 0 and start_col == 0:
+        rectangle_sum = int(matrix[end_row][end_col])
+        if end_row - 1 >= 0:
+            rectangle_sum += sum_arr[end_row - 1][end_col]
+        if end_col - 1 >= 0:
+            rectangle_sum += sum_arr[end_row][end_col - 1]
+        if end_row - 1 >= 0 and end_col - 1 >= 0:
+            rectangle_sum -= sum_arr[end_row - 1][end_col - 1]
+        sum_arr[end_row][end_col] = rectangle_sum
+    else:
+        rectangle_sum = sum_arr[end_row][end_col]
+        if start_row - 1 >= 0:
+            rectangle_sum -= sum_arr[start_row - 1][end_col]
+        if start_col - 1 >= 0:
+            rectangle_sum -= sum_arr[end_row][start_col - 1]
         if start_row - 1 >= 0 and start_col - 1 >= 0:
-            rectangle_sum -= sums_dict.get((start_row - 1, end_col))
-            rectangle_sum -= sums_dict.get((end_row, start_col - 1))
-            rectangle_sum += sums_dict.get((start_row - 1, start_col - 1))
-        elif start_row - 1 >= 0:
-            rectangle_sum -= sums_dict.get((start_row - 1, end_col))
-        elif start_col - 1 >= 0:
-            rectangle_sum -= sums_dict.get((end_row, start_col - 1))
-        return rectangle_sum
-    matrix_sum = 0
-    for row in range(end_row + 1):
-        for col in range(end_col + 1):
-            matrix_sum += int(matrix[row][col])
-    sums_dict.update({(end_row, end_col): matrix_sum})
-    return matrix_sum
-
+            rectangle_sum += sum_arr[start_row - 1][start_col - 1]
+    return rectangle_sum
 
 matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
 print(maximal_rectangle(matrix))
@@ -40,3 +44,7 @@ print(maximal_rectangle(matrix))
 
 matrix = [["0", "0"], ["0", "0"]]
 print(maximal_rectangle(matrix))
+
+matrix = [["0", "1"]]
+print(maximal_rectangle(matrix))
+
