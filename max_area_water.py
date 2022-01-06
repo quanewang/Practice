6b3cd3611be18715e835575cf93fddcce6ff32c7
Royal4224 <roytwang1234@gmail.com>
2022-01-06 11:10:14 -0800

    Update max_area_water.py

--- max_area_water.py
+++ max_area_water.py
@@ -1,1 +1,24 @@
 def max_area(height):
+    left = 0
+    right = len(height) - 1
+    maximum_area = 0
+    while left < right:
+        if height[left] < height[right]:
+            current_area = height[left] * (right - left)
+            left += 1
+        else:
+            current_area = height[right] * (right - left)
+            right -= 1
+        if current_area > maximum_area:
+            maximum_area = current_area
+    return maximum_area
+
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(max_area(height))
+
+height = [1, 1]
+print(max_area(height))
+
+height = [0, 1]
+print(max_area(height))
