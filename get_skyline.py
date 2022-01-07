720c40a6af42378613afb209dac59850164f55c1
Royal4224 <roytwang1234@gmail.com>
2022-01-07 13:02:53 -0800

    Update get_skyline.py

--- get_skyline.py
+++ get_skyline.py
@@ -4,10 +4,13 @@ import heapq
 def get_skyline(buildings):
     skyline = []
     max_heap = []
-    for i in range(len(buildings)):
+    i = 0
+    while i < len(buildings):
         building = buildings[i]
         while max_heap and building[0] > max_heap[0][1]:
             largest_building = heapq.heappop(max_heap)
+            while max_heap and largest_building[1] > max_heap[0][1]:
+                heapq.heappop(max_heap)
             if max_heap:
                 next_building = max_heap[0]
                 if largest_building[1] < next_building[1]:
@@ -25,16 +28,17 @@ def get_skyline(buildings):
         while j < len(buildings) and building[0] == buildings[j][0]:
             if buildings[j][2] > max_height:
                 max_height = buildings[j][2]
-            heapq.heappush(max_heap, [-buildings[j][2], buildings[j][1]])
             j += 1
 
-        if not max_heap or building[2] > -max_heap[0][0]:
+        if not max_heap or max_height > -max_heap[0][0]:
             skyline.append([building[0], max_height])
-
-        heapq.heappush(max_heap, [-building[2], building[1]])
+        heapq.heappush(max_heap, [-max_height, building[1]])
+        i += 1
 
     while max_heap:
         largest_building = heapq.heappop(max_heap)
+        while max_heap and largest_building[1] > max_heap[0][1]:
+            heapq.heappop(max_heap)
         if max_heap:
             next_building = max_heap[0]
             if largest_building[1] < next_building[1]:
@@ -48,57 +52,6 @@ def get_skyline(buildings):
             skyline.append([largest_building[1], 0])
 
     return skyline
-
-
-def check_heap(heap_ends, building_start, building_height):
-    adjusted_start = building_start
-    for end_height_pair in heap_ends:
-        start, end, height = end_height_pair[0], end_height_pair[1], end_height_pair[2]
-
-        if end > building_start and height > building_height:
-            if end > adjusted_start:
-                adjusted_start = end
-
-        if end < building_start:
-            heap_ends.remove(end_height_pair)
-
-    return adjusted_start
-
-
-def merge_blocks(buildings):
-    copy = []
-    previous_block = None
-    for i in range(len(buildings) - 1):
-        if previous_block is None:
-            previous_block = merge(buildings[i], buildings[i + 1])
-        else:
-            previous_block = merge(previous_block, buildings[i])
-        if len(previous_block) == 2:
-            copy.append(previous_block[0])
-            copy.append(previous_block[1])
-            previous_block = buildings[i + 1]
-    copy.append(previous_block)
-    return copy
-
-
-def merge(first, second):
-    if first[0] == second[0] and first[1] == second[1]:
-        height = max(first[2], second[2])
-        return [first[0], first[1], height]
-
-    elif first[0] == second[0]:
-        if first[2] <= second[2]:
-            return second
-        else:
-            return first, [first[1], second[1], second[2]]
-
-    elif first[1] == second[1]:
-        if first[2] >= second[2]:
-            return [first]
-        else:
-            return [first[0], second[0], first[2]], second
-
-    return first, second
 
 
 buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
@@ -118,4 +71,21 @@ buildings = [[1, 2, 1], [1, 2, 2], [1, 2, 3], [2, 6, 10], [3, 7, 15], [4, 10, 19
 print(get_skyline(buildings))
 
 buildings = [[3, 7, 8], [3, 8, 7], [3, 9, 6], [3, 10, 5], [3, 11, 4], [3, 12, 3], [3, 13, 2], [3, 14, 1]]
+print(get_skyline(buildings))
+
+buildings = [[1, 38, 219], [2, 19, 228], [2, 64, 106], [3, 80, 65], [3, 84, 8], [4, 12, 8], [4, 25, 14], [4, 46, 225],
+             [4, 67, 187], [5, 36, 118], [5, 48, 211], [5, 55, 97], [6, 42, 92], [6, 56, 188], [7, 37, 42], [7, 49, 78],
+             [7, 84, 163], [8, 44, 212], [9, 42, 125], [9, 85, 200], [9, 100, 74], [10, 13, 58], [11, 30, 179],
+             [12, 32, 215], [12, 33, 161], [12, 61, 198], [13, 38, 48], [13, 65, 222], [14, 22, 1], [15, 70, 222],
+             [16, 19, 196], [16, 24, 142], [16, 25, 176], [16, 57, 114], [18, 45, 1], [19, 79, 149], [20, 33, 53],
+             [21, 29, 41], [23, 77, 43], [24, 41, 75], [24, 94, 20], [27, 63, 2], [31, 69, 58], [31, 88, 123],
+             [31, 88, 146], [33, 61, 27], [35, 62, 190], [35, 81, 116], [37, 97, 81], [38, 78, 99], [39, 51, 125],
+             [39, 98, 144], [40, 95, 4], [45, 89, 229], [47, 49, 10], [47, 99, 152], [48, 67, 69], [48, 72, 1],
+             [49, 73, 204], [49, 77, 117], [50, 61, 174], [50, 76, 147], [52, 64, 4], [52, 89, 84], [54, 70, 201],
+             [57, 76, 47], [58, 61, 215], [58, 98, 57], [61, 95, 190], [66, 71, 34], [66, 99, 53], [67, 74, 9],
+             [68, 97, 175], [70, 88, 131], [74, 77, 155], [74, 99, 145], [76, 88, 26], [82, 87, 40], [83, 84, 132],
+             [88, 99, 99]]
+print(get_skyline(buildings))
+
+buildings = [[0,5,7],[5,10,7],[5,10,12],[10,15,7],[15,20,7],[15,20,12],[20,25,7]]
 print(get_skyline(buildings))
