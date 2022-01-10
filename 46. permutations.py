c4d585b78011d19fe27ad1203281aa726e414aac
Royal4224 <roytwang1234@gmail.com>
2022-01-10 15:15:06 -0800

    Create 46. permutations.py

---
+++ 46. permutations.py
@@ -0,0 +1,24 @@
+def permutations(nums):
+    perms = []
+    if len(nums) == 1:
+        return [nums]
+    for i in range(len(nums)):
+        removed = nums.pop(i)
+        copy = nums.copy()
+        nums.insert(i, removed)
+        get_perms = permutations(copy)
+        for perm in get_perms:
+            perm.append(removed)
+        perms.extend(get_perms)
+
+    return perms
+
+
+nums = [1, 2, 3]
+print(permutations(nums))
+
+nums = [0, 1]
+print(permutations(nums))
+
+nums = [1]
+print(permutations(nums))
