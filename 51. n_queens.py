b378d4e1bcf9cb84bc32d1ffe1c0a337a6fdf67f
Royal4224 <roytwang1234@gmail.com>
2022-01-13 00:52:32 -0800

    Create 51. n_queens.py

---
+++ 51. n_queens.py
@@ -0,0 +1,67 @@
+def n_queens(n):
+    solutions = []
+    valid_squares = set()
+    valid_squares.copy()
+    board = []
+    for i in range(0, n):
+        board.append("." * n)
+        for j in range(n):
+            valid_squares.add((i, j))
+
+    for j in range(len(board)):
+        copy = board.copy()
+        valid_copy = valid_squares.copy()
+        solution = set()
+        solution = try_square(0, j, copy, valid_copy, solution)
+        if len(solution) == 4:
+            for (i, j) in solution:
+                temp = copy[i][0:j]
+                copy[i] = temp + "Q" + copy[i][j:len(copy[i])]
+            solutions.append(copy)
+    return solutions
+
+
+def try_square(i, j, copy, valid_squares, solution, count=1):
+    validate_squares(i, j, copy, valid_squares)
+    solution.add((i, j))
+    if len(solution) == 4 or not valid_squares:
+        return solution
+    for j in range(len(copy)):
+        if (i + 1, j) in valid_squares:
+            sol = try_square(i + 1, j, copy, valid_squares.copy(), solution.copy(), count + 1)
+            if len(sol) == 4:
+                return sol
+
+    return set()
+
+
+def validate_squares(i, j, copy, valid_squares):
+    if not validate_squares:
+        return
+    for row in range(len(copy)):
+        if (row, j) in valid_squares:
+            valid_squares.remove((row, j))
+
+    for col in range(len(copy)):
+        if (i, col) in valid_squares:
+            valid_squares.remove((i, col))
+
+    k = i + 1
+    l = j + 1
+    while k < len(copy) and l < len(copy):
+        if (k, l) in valid_squares:
+            valid_squares.remove((k, l))
+        k += 1
+        l += 1
+
+    k = i + 1
+    l = j - 1
+    while k < len(copy) and l >= 0:
+        if (k, l) in valid_squares:
+            valid_squares.remove((k, l))
+        k += 1
+        l -= 1
+    return
+
+
+print(n_queens(4))
