1bec64217e58043bc5efe26591da72011ef5bb35
Royal4224 <roytwang1234@gmail.com>
2022-01-12 16:22:35 -0800

    Update 30. find_substring.py

--- 30. find_substring.py
+++ 30. find_substring.py
@@ -1,35 +1,35 @@
 def find_substring(s, words):
     indexes = []
-    length = len(words[0]) * len(words)
+    word_len = len(words[0])
+    length = word_len * len(words)
     i = 0
     word_dict = {}
+
     for word in words:
         if word_dict.get(word) is None:
             word_dict.update({word: 1})
         else:
             word_dict.update({word: word_dict.get(word) + 1})
+
     while i + length - 1 < len(s):
         sub = s[i: i + length]
-        j, is_string = check_string(sub, word_dict.copy(), 0, len(words[0]), word_dict)
-        if is_string:
+        if check_string(sub, word_dict.copy(), word_len):
             indexes.append(i)
         i += 1
 
     return indexes
 
 
-def check_string(sub, dict_copy, i, word_len, word_dict):
+def check_string(sub, dict_copy, word_len):
+    i = 0
     while i < len(sub):
         str = sub[i: i + word_len]
         if dict_copy.get(str) is None or dict_copy.get(str) == 0:
-            if i == 0 or word_dict.get(str) is not None:
-                return word_len, False
-            else:
-                return i, False
+            return False
         else:
             dict_copy.update({str: dict_copy.get(str) - 1})
         i += word_len
-    return word_len, True
+    return True
 
 
 
