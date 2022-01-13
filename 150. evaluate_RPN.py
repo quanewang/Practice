27e5bc579604e486d5e420a3841772a0bf4ed304
Royal4224 <roytwang1234@gmail.com>
2022-01-12 18:11:51 -0800

    Update 150. evaluate_RPN.py

--- 150. evaluate_RPN.py
+++ 150. evaluate_RPN.py
@@ -18,12 +18,49 @@ def eval_RPN(tokens):
     return stack[0]
 
 
-tokens = ["2","1","+","3","*"]
+tokens = ["2", "1", "+", "3", "*"]
 print(eval_RPN(tokens))
 
-tokens = ["4","13","5","/","+"]
+tokens = ["4", "13", "5", "/", "+"]
 print(eval_RPN(tokens))
 
-tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
 print(eval_RPN(tokens))
 
+tokens = ["a+b*d+c"]
+
+
+def infix_to_postfix(expression):
+    operators = []
+    operands = []
+    for char in expression:
+        if char == '*' or char == '+':
+            operators.append(char)
+        else:
+            operands.append(char)
+
+    while operators:
+        operator = operators.pop(len(operators) - 1)
+        e_0 = operands.pop(len(operands) - 1)
+        e_1 = operands.pop(len(operands) - 1)
+        if operator == '*' and len(e_0) != 1:
+            operands.insert(len(operands), e_1 + e_0[0] + operator + e_0[1:len(e_0)])
+        else:
+            operands.insert(len(operands), e_1 + e_0 + operator)
+    return operands.pop(0)
+
+
+expression = "1+2*3"
+print(infix_to_postfix(expression))
+
+expression = "1*2*3"
+print(infix_to_postfix(expression))
+
+expression = "1*2*3+4"
+print(infix_to_postfix(expression))
+
+expression = "1+2+3+4"
+print(infix_to_postfix(expression))
+
+expression = "1*2*3*4"
+print(infix_to_postfix(expression))
