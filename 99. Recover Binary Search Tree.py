48afff175b3dd3a37fc907f32f546fab70eaaa4c
Royal4224 <roytwang1234@gmail.com>
2022-02-16 21:47:08 -0600

    Update 99. Recover Binary Search Tree.py

--- 99. Recover Binary Search Tree.py
+++ 99. Recover Binary Search Tree.py
@@ -1,30 +1,48 @@
 class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def recoverTree(root):
-    validate(root)
+    validate(root, [])
 
 
-def validate(node, previous_nodes = None):
-    if node.left:
-        previous_nodes.append((node, 0))
-        validate(node.left, previous_nodes)
-
-    if node.right:
-        previous_nodes.append((node, 1))
-        validate(node.right, previous_nodes)
-
-    for parent, subtree in previous_nodes:
+def validate(node, previous_nodes):
+    for i in range(len(previous_nodes)):
+        parent, subtree = previous_nodes[i]
         if subtree == 0:
-            if parent.val > node.val:
-                pass  # swap
+            if parent.val < node.val:  # swap
+                temp = parent.val
+                parent.val = node.val
+                node.val = temp
+
+                return True
 
         else:
-            if parent.val < node.val:
-                pass  # swap
+            if parent.val > node.val:  # swap
+                temp = parent.val
+                parent.val = node.val
+                node.val = temp
+
+                return True
+        
+    copy = previous_nodes.copy()
+    if node.left:
+        copy.append((node, 0))
+        if validate(node.left, copy):
+            return True
+
+    copy = previous_nodes.copy()
+    if node.right:
+        copy.append((node, 1))
+        if validate(node.right, copy):
+            return True
+
+
+
+    return False
+
 
 
