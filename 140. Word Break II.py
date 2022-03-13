1b8ca724b2a14cb7c136d93afbb4a2ed4b596202
Royal4224 <roytwang1234@gmail.com>
2022-03-12 19:00:11 -0600

    Update 140. Word Break II.py

--- 140. Word Break II.py
+++ 140. Word Break II.py
@@ -1,5 +1,5 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+    def wordBreak(self, s: str, word_dict):
         word_list = []
         if s == "":
             return [""]
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(s)):
             word = s[0:i + 1]
 
-            if word in wordDict:
-                valid_phrases = self.wordBreak(s[i + 1: len(s)], wordDict)
+            if word in word_dict:
+                valid_phrases = self.wordBreak(s[i + 1: len(s)], word_dict)
 
                 if valid_phrases is not None:
                     for v_p in valid_phrases:
